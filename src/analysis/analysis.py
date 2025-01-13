dbe0d3aa798476c9b2b5a376a5ead7aee86d8c38
mike <mxu@capvision.com>
2025-01-14 00:34:27 +0800

    add comments

--- src/analysis/analysis.py
+++ src/analysis/analysis.py
@@ -2,8 +2,10 @@ from typing import List, Tuple, Optional, Set
 
 import hanlp
 
-from src.analysis.models import AnalysisResponse, Term, NamedEntity, FineCoarseAnalysisResponse
+from src.analysis.models import AnalysisResponse, Term, NamedEntity, \
+    FineCoarseAnalysisResponse
 
+# TOKEN_WITH_SPAN = "token_with_span"
 TOKEN = "token"
 POS_CTB = "pos_ctb"
 POS_PKU = "pos_pku"
@@ -24,6 +26,15 @@ __tok_fine = hanlp.load(hanlp.pretrained.tok.FINE_ELECTRA_SMALL_ZH)
 # 杭州甘其食 可能更应该算作两个 token.
 __tok_coarse = hanlp.load(hanlp.pretrained.tok.COARSE_ELECTRA_SMALL_ZH)
 
+# HanLP支持输出每个单词在文本中的原始位置，以便用于搜索引擎等场景。
+# 在词法分析中，非语素字符（空格、换行、制表符等）会被剔除，此时需要额外的位置信息才能定位每个单词
+# 通过 config.output_spans = True
+# 返回格式为三元组（单词，单词的起始下标，单词的终止下标），下标以字符级别计量。
+# https://github.com/hankcs/HanLP/issues/1802#issuecomment-1399534301
+# 但由于我们会把文本先拆成句子，此时返回位置是句子的位置，因此就作用不大了
+# __tok_fine.config.output_spans = True
+# __tok_coarse.config.output_spans = True
+
 __ner = hanlp.load(hanlp.pretrained.ner.MSRA_NER_ELECTRA_SMALL_ZH)
 __pos_ctb9 = hanlp.load(hanlp.pretrained.pos.CTB9_POS_ELECTRA_SMALL)
 __pos_pku = hanlp.load(hanlp.pretrained.pos.PKU_POS_ELECTRA_SMALL)
@@ -38,10 +49,17 @@ def __zip_terms(*args) -> List[Term]:
     res = []
     for tok, pos9, posp in zip(token, pos_ctb9, pos_pku):
         for t, p9, pp in zip(tok, pos9, posp):
+            # token, start, end = t
+            # res.append(Term(
+            #     token=token,
+            #     pos_ctb=p9,
+            #     pos_pku=pp,
+            #     span=(start, end)
+            # ))
             res.append(Term(
                 token=t,
                 pos_ctb=p9,
-                pos_pku=pp
+                pos_pku=pp,
             ))
     return res
 
@@ -59,6 +77,15 @@ def _filter_terms(
     return res
 
 
+# def __remove_span(token_with_span):
+#     res = []
+#     for items in token_with_span:
+#         res.append([token for token, start, end in items])
+#     return res
+
+# 注意，因为分句会丢失上下文信息，所以可以在一定程度上对分词结果有不好的影响
+# e.g. 2）大众点评、天猫、百度负责医美广告的； 3）更美、美呗等竞对；
+# 在粗分情况下 2)和3) 在不在一行，决定了“更美”“美呗”能否被分对。。
 __fine_analysis_pipeline = hanlp.pipeline() \
     .append(hanlp.utils.rules.split_sentence, output_key='sentences') \
     .append(__tok_fine, input_key='sentences', output_key=TOKEN) \
@@ -66,7 +93,19 @@ __fine_analysis_pipeline = hanlp.pipeline() \
     .append(__pos_pku, input_key=TOKEN, output_key=POS_PKU) \
     .append(__ner, input_key=TOKEN, output_key=NAMED_ENTITIES) \
     .append(__sum, input_key=NAMED_ENTITIES, output_key=NAMED_ENTITIES) \
-    .append(__zip_terms, input_key=(TOKEN, POS_CTB, POS_PKU), output_key=TERMS)
+    .append(__zip_terms, input_key=(TOKEN, POS_CTB, POS_PKU),
+            output_key=TERMS)
+
+# __fine_analysis_pipeline_with_span = hanlp.pipeline() \
+#     .append(hanlp.utils.rules.split_sentence, output_key='sentences') \
+#     .append(__tok_fine, input_key='sentences', output_key=TOKEN_WITH_SPAN) \
+#     .append(__remove_span, input_key=TOKEN_WITH_SPAN, output_key=TOKEN) \
+#     .append(__pos_ctb9, input_key=TOKEN, output_key=POS_CTB) \
+#     .append(__pos_pku, input_key=TOKEN, output_key=POS_PKU) \
+#     .append(__ner, input_key=TOKEN, output_key=NAMED_ENTITIES) \
+#     .append(__sum, input_key=NAMED_ENTITIES, output_key=NAMED_ENTITIES) \
+#     .append(__zip_terms, input_key=(TOKEN_WITH_SPAN, POS_CTB, POS_PKU),
+#             output_key=TERMS)
 
 __coarse_analysis_pipeline = hanlp.pipeline() \
     .append(hanlp.utils.rules.split_sentence, output_key='sentences') \
@@ -75,7 +114,8 @@ __coarse_analysis_pipeline = hanlp.pipeline() \
     .append(__pos_pku, input_key=TOKEN, output_key=POS_PKU) \
     .append(__ner, input_key=TOKEN, output_key=NAMED_ENTITIES) \
     .append(__sum, input_key=NAMED_ENTITIES, output_key=NAMED_ENTITIES) \
-    .append(__zip_terms, input_key=(TOKEN, POS_CTB, POS_PKU), output_key=TERMS)
+    .append(__zip_terms, input_key=(TOKEN, POS_CTB, POS_PKU),
+            output_key=TERMS)
 
 
 def _filter_named_entities(
